097abc867dd70cb80c65ebb9aa85c5dde4c67883
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-05-06 11:35:11 -0500

    updating some tests

--- src/app/calculator/calculator.service.spec.ts
+++ src/app/calculator/calculator.service.spec.ts
@@ -2,36 +2,39 @@ import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
 import { Calculator } from './calculator.model';
 
-
-
 describe('CalcService', () => {
-    let service: CalculatorService;
+  let service: CalculatorService;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({})
-        service = TestBed.inject(CalculatorService)
-    })
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CalculatorService);
+  });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    })
-
-    it('monthly interest should return 833 so that pie-chart has correct initial value', () => {
-        expect(service.monthlyInterest).toEqual(833)
-    })
-
-    it('monthly principle should return 240 so that pie-chart has correct initial value', () => {
-        expect(service.monthlyPrinciple).toEqual(240)
-    })
-
-//    it('updateInputs should return the correct starting values: [250000, 50000, 20, 30, 5, 0, 1.5, 0, 0, "", ""]', () => {
-//     const intialMortgageModel = new Calculator(250000, 50000, 20, 30, 5, 0, 1.5, 0, 0, "", "")
-//     service.updateInputs = jest.fn()
-    
-    
-//     expect(service.updateInputs).toHaveBeenCalledWith(intialMortgageModel)
-// })
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
 
+  it('monthlyPayment, monthlyPrinciple, and monthlyInterest should all return the correct values on startup', () => {
+    const intialMortgageModel: Calculator = {
+      homePrice: 250000,
+      downPayment: 50000,
+      downPaymentPercentage: 20,
+      loanProgram: 30,
+      interestRate: 5,
+      propertyTax: 0,
+      propertyTaxRate: 1.5,
+      homeInsurance: 0,
+      hoaDues: 0,
+      pmi: '',
+      taxesAndInsurance: '',
+    };
+    service.updateInputs(intialMortgageModel);
+    expect(service.homePrice).toEqual(250000)
+    expect(service.monthlyPayment).toEqual('1073.64')
+    expect(service.monthlyPrinciple).toEqual(240.30991269094613) //maybe use toFixed(0) to make this look nicer
+    expect(service.monthlyInterest).toEqual(833.3333333333334)
+  });
 
-})
+
+});
